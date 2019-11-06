9b152fae2bd61704549ed9e96324be63ff92f68d
Murilo Moraes <murilo.mm.moraes@gmail.com>
2019-11-06 11:30:26 -0300

    Added suggestions and keyboard

--- bot/gov-bot.php
+++ bot/gov-bot.php
@@ -29,7 +29,7 @@ switch ($text)
         $response = 'Bem vindo à plataforma de serviços do governo brasileiro.';
         file_get_contents($bot."/sendmessage?chat_id=$chat_id&text=$response");
 
-        $result = json_decode(file_get_contents("https://hs2019st.com/govbr/solr-select.php?q='".$text."'&fl=id,nome_s&rows=4"),TRUE);
+        $result = json_decode(file_get_contents("https://hs2019st.com/govbr/solr-select.php?q=*:*fl=id,nome_s&rows=4"),TRUE);
         $response = 'Esses são os serviços mais acessados ultimamente:';
         file_get_contents($bot."/sendmessage?chat_id=$chat_id&text=$response");
 
@@ -48,8 +48,45 @@ switch ($text)
         break;
     default:
         $result = json_decode(file_get_contents("https://hs2019st.com/govbr/solr-select.php?q='".$text."'&fl=id,nome_s&rows=4"),TRUE);
-        $response = $result['response']['docs'][0]['nome_s'];
-        file_get_contents($bot."/sendmessage?chat_id=$chat_id&text=$response");
+        $total = $result['response']['numFound'];
+
+        if(!$total)
+        {
+            $response = "Não foram encontrados resultados para a sua busca.";
+            file_get_contents($bot."/sendmessage?chat_id=$chat_id&text=$response");
+        }
+
+        if($total == 1)
+        {
+            $response = '<a href="https://www.google.com">'.$result['response']['docs'][0]['nome_s'].'</a>';
+            file_get_contents($bot."/sendmessage?chat_id=$chat_id&parse_mode=HTML&text=$response");
+        }
+
+        if($total > 1 )
+        {
+            $response = "Foram encontrados $total serviços.";
+            file_get_contents($bot."/sendmessage?chat_id=$chat_id&text=$response");
+            $response = '<a href="https://www.google.com">'.$result['response']['docs'][0]['nome_s'].'</a>';
+            file_get_contents($bot."/sendmessage?chat_id=$chat_id&parse_mode=HTML&text=$response");
+            $response = '<a href="https://www.google.com">'.$result['response']['docs'][1]['nome_s'].'</a>';
+            file_get_contents($bot."/sendmessage?chat_id=$chat_id&parse_mode=HTML&text=$response");
+            $response = '<a href="https://www.google.com">'.$result['response']['docs'][2]['nome_s'].'</a>';
+            file_get_contents($bot."/sendmessage?chat_id=$chat_id&parse_mode=HTML&text=$response");
+            $response = '<a href="https://www.google.com">'.$result['response']['docs'][3]['nome_s'].'</a>';
+            file_get_contents($bot."/sendmessage?chat_id=$chat_id&parse_mode=HTML&text=$response");
+
+            $result = json_decode(file_get_contents("https://hs2019st.com/govbr/solr-suggest.php?suggest=true&suggest.build=true&suggest.dictionary=nomeSuggester&wt=json&suggest.count=4&suggest.q='$text'"),TRUE);
+            $response = 'Deseja refinar a sua busca? Tente um dos termos sugeridos:';
+            $keyboard = "[ [' ".$result['suggest']['nomeSuggester'][$text]['suggestions'][0]['nome_s']." '],
+                            [' ".$result['suggest']['nomeSuggester'][$text]['suggestions'][1]['nome_s']." '],
+                            [' ".$result['suggest']['nomeSuggester'][$text]['suggestions'][2]['nome_s']." '],
+                            [' ".$result['suggest']['nomeSuggester'][$text]['suggestions'][3]['nome_s']." '],
+            ]";
+            $reply_keyboard = "{'keyboard': $keyboard, 'one_time_keyboard': False, 'resize_keyboard': True}";
+
+            file_get_contents($bot."/sendmessage?chat_id=$chat_id&reply_markup=$reply_keyboard&text=$response");
+        }
+
         break;
 }
 
