8137b265ec8c60c6562190cdc6c8965a9eda300c
Murilo Moraes <murilo.mm.moraes@gmail.com>
2019-11-06 09:57:26 -0300

    Added bot

--- bot/gov-bot.php
+++ bot/gov-bot.php
@@ -31,7 +31,7 @@ switch ($text)
         break;
     default:
         $result = json_decode(file_get_contents("http://hs2019st.com:8983/solr/servicos/select?q=*:*&views_i%20desc&fq=palavra_chave_ss:'$text'"),TRUE);
-        $response = $result;
+        $response = $result['response']['docs'][0]['nome_s'];
         break;
 }
 
